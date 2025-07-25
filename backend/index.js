ca8121379f4d7c98c689df543c25a1f60333dc9e
sagaradreddy <113368393+sagaradreddy@users.noreply.github.com>
2025-07-25 12:14:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 8000
 
 // const app = express();
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://realtimechatapp-frontend-ipfu.onrender.com",
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   credentials:true,
   allowedHeaders: ['Content-Type', 'Authorization']
